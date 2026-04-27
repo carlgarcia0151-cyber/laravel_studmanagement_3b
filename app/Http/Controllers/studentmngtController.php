036b72b99abda37ca98107754aaf4a8803e6e4a2
renz123095 <pandiojames18@gmail.com>
2026-04-27 14:24:57 +0800

    My first index

---
+++ app/Http/Controllers/studentmngtController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+
+class studentmngtController extends Controller
+{
+    public function index()
+    {
+        $users = User::paginate();
+
+        return view('users.index', compact('users'));
+    }
+}
